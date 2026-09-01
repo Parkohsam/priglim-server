c1458d9f4ce0fbb04e95b44e227c3e99cfc8ce61
PARKOHSAM <ajaoadediran@gmail.com>
2026-09-01 15:41:24 +0100

    Check Firebase environment variables

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,9 +1,9 @@
 const admin = require("firebase-admin");
 
 const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  projectId: process.env.project_id,
+  clientEmail: process.env.FIREBASE_client_email,
+  privateKey: process.env.private_key.replace(/\\n/g, "\n"),
 };
 
 admin.initializeApp({
